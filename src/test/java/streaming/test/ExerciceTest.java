6aec6794c7c2e641b6b4be3728cd8d50f116e2f7
nercronus <36480674+nercronus@users.noreply.github.com>
2018-04-13 16:31:46 +0200

    projet HS MAJ

--- src/test/java/streaming/test/ExerciceTest.java
+++ src/test/java/streaming/test/ExerciceTest.java
@@ -9,6 +9,7 @@ package streaming.test;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import org.eclipse.equinox.internal.app.Messages;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.data.domain.Persistable;
@@ -19,11 +20,59 @@ import streaming.entity.Film;
  * @author Formation
  */
 public class ExerciceTest {
+    @Test   //12. Le nombre de films policiers réalisés à la fois par les 2 frères Coen, et interprétés par Steve Buscemi
+    public void req12(){
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        Query query = em.createQuery("select count (f) from Film f join f.genre g join f.realisateurs r1 join f.realisateurs r2 join f.acteurs a where g.nom='policiers'and r1.nom='Coen' and r1.prenom='Joel' and r2.nom='Coen' and r2.prenom='Ethan' and a.nom='Buscemi' and a.prenom='Steve'");
+        long nbfilm = (long) query.getSingleResult();
+        System.out.println(nbfilm);
+        //assertEquals(9L, nbfilm);
+    }
+    
+    @Test   
+    public void req11(){
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        Query query = em.createQuery("select count(f) from Film f join f.realisateurs r1 join f.realisateurs r2 join f.acteurs a where r1.nom='Coen' and r1.prenom='Joel' and r2.nom='Coen' and r2.prenom='Ethan' and a.nom='Buscemi' and a.prenom='Steve'");
+        long nbfilm = (long) query.getSingleResult();
+        
+        
+        assertEquals(2L, nbfilm);
+    }
+    
+    @Test  
+    public void req10(){
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        Query query = em.createQuery("select count(f) from Film f join f.realisateurs r1 join f.realisateurs r2 where r1.nom='Coen'and r1.prenom='Joel' and r2.nom='Coen'and r2.prenom='Ethan'");
+        long nbfilm = (long) query.getSingleResult();
+        
+        assertEquals(2L, nbfilm);
+    }
+    
+    @Test
+    public void req9(){
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        Query query = em.createQuery("select count(f) from Film f join f.realisateurs r where r.nom='Coen' and r.prenom='Joel'");
+        long nbfilm = (long) query.getSingleResult();
+       
+        assertEquals(2L, nbfilm);
+    }
+    
+    @Test  
+    public void req8(){
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        Query query = em.createQuery("select f from Film f join f.genre g join f.pays p join f.realisateurs r where g.nom='Horreur' and p.nom='UK' and r.nom='Polanski'");
+        Film f = (Film) query.getSingleResult();
+        
+        assertEquals("Le bal des vampires", f.getTitre());
+    }
+    
     @Test
     public void req7(){
         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
-        Query query = em.createQuery("select count(f) from Film join f.acteur a join f.realisateurs r where a.nom='polanski and r.nom='polanski");
+        Query query = em.createQuery("select count(f) from Film f join f.acteurs a join f.realisateurs r where a.nom='Polanski' and r.nom='Polanski'");
+        long gg = (long) query.getSingleResult();
         
+        assertEquals(1L, gg);
     }
     
     @Test
